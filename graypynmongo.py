e42df2f04d849251eb2d7a269328c1bd3ca6c934
Stepan Serebryakov <serebryakov0906@gmail.com>
2021-01-27 22:00:13 +0600

    test script completed

--- graypynmongo.py
+++ graypynmongo.py
@@ -4,7 +4,7 @@
 import graypy
 import logging
 from pymongo import MongoClient
-
+# https://docs.mongodb.com/manual/reference/operator/aggregation/
 mydb_name = "pymongobd_usage";
 client = MongoClient("mongodb://localhost:27017");
 if mydb_name in client.list_database_names():
@@ -13,35 +13,53 @@ if mydb_name in client.list_database_names():
 dtbase = client[mydb_name];
 countries = dtbase["countries"];
 one_country_data = \
-    { "country_name" : "Mongolia"  ,"off_language"  : "Mongolian", "million_sq_km" :  "1.55"};
+    { "country_name" : "Mongolia"  ,"off_language"  : "Mongolian", "million_sq_km" :  1.55};
 insert_result = countries.insert_one(one_country_data);
-print('single insertion: {0}'.format(insert_result.inserted_id))
+print('single insertion: {0}\n'.format(insert_result.inserted_id));
 countries_data = [
-    { "country_name" : "Russia"    ,"off_language"  : "Russian"  , "million_sq_km" : "16.38"},
-    { "country_name" : "Canada"    ,"off_language"  : "English"  , "million_sq_km" :  "9.09"},
-    { "country_name" : "Australia" ,"off_language"  : "English"  , "million_sq_km" :  "7.68"},
-    { "country_name" : "China"     ,"off_language"  : "Mandarin" , "million_sq_km" :  "9.33"},
-    { "country_name" : "Kazakhstan","off_language"  : "Kazakh"   , "million_sq_km" :  "2.70"}];
+    { "country_name" : "Russia"    ,"off_language"  : "Russian"  , "million_sq_km" : 16.38},
+    { "country_name" : "Canada"    ,"off_language"  : "English"  , "million_sq_km" :  9.09},
+    { "country_name" : "Australia" ,"off_language"  : "English"  , "million_sq_km" :  7.68},
+    { "country_name" : "China"     ,"off_language"  : "Mandarin" , "million_sq_km" :  9.33},
+    { "country_name" : "Kazakhstan","off_language"  : "Kazakh"   , "million_sq_km" :  2.70}];
 insert_result = countries.insert_many(countries_data);
-print('multiple insertion: {0}'.format(insert_result.inserted_ids))
-
-
-
-
-exit();
-
+print('multiple insertion: {0}\n'.format(insert_result.inserted_ids));
+print('------------------------------------------------------------');
+eng_lang = countries.find({"off_language"  : "English"});
+print('countries found: {0}\n'.format(eng_lang.count()));
+for country in eng_lang: 
+    print(country);
+# Field Update Operators!!!
+countries.update({"country_name":"Russia"}, {"$set": { "million_sq_km": 16.37 }})
+print(countries.find_one({"country_name":"Russia"}));
+countries.update({"country_name":"Russia"}, {"$inc": { "million_sq_km": 10.00 }})
+print(countries.find_one({"country_name":"Russia"}));
+print('------------------------------------------------------------');
+myquery = { "million_sq_km": { "$gt": 7.00 } };
+for country in countries.find(myquery):
+    print(country)
+    
 my_logger = logging.getLogger("test_logger")
-my_logger.setLevel(logging.DEBUG)
-
-
 handler = graypy.GELFUDPHandler("monitoring.igor.kz", 5555)
 handler.facility = "stepan";
-
-
-my_logger.extra_fields = True
-my_logger.level_names = True
+handler.level_names = True;
 my_adapter = logging.LoggerAdapter(logging.getLogger('test_logger'),{'username': 'Stepan'})
+my_logger.addHandler(handler);
+#my_logger.setLevel(logging.WARNING);
+#my_logger.setLevel(logging.INFO);
+#my_logger.setLevel(logging.ERROR);
+#my_logger.setLevel(logging.DEBUG);
 
-my_logger.addHandler(handler)
+c_begin_countries = countries.find({"country_name":{"$regex":"^C"}});
+if (c_begin_countries.count() > 1):
+    my_logger.setLevel(logging.INFO);
+    my_logger.info('several countires were found');
 
-my_logger.debug('custom_logging_level')
+
+
+
+
+
+
+
+
