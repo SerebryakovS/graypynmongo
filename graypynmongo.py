5d8d66fef21458b64ce7f0036dfce5507abaf50a
Stepan Serebryakov <serebryakov0906@gmail.com>
2021-01-27 20:40:46 +0600

    added pymongo operations

--- graypynmongo.py
+++ graypynmongo.py
@@ -5,24 +5,27 @@ import graypy
 import logging
 from pymongo import MongoClient
 
-
-
 mydb_name = "pymongobd_usage";
 client = MongoClient("mongodb://localhost:27017");
 if mydb_name in client.list_database_names():
     client.drop_database(mydb_name);
     print("database has been dropped");
 dtbase = client[mydb_name];
+countries = dtbase["countries"];
+one_country_data = \
+    { "country_name" : "Mongolia"  ,"off_language"  : "Mongolian", "million_sq_km" :  "1.55"};
+insert_result = countries.insert_one(one_country_data);
+print('single insertion: {0}'.format(insert_result.inserted_id))
+countries_data = [
+    { "country_name" : "Russia"    ,"off_language"  : "Russian"  , "million_sq_km" : "16.38"},
+    { "country_name" : "Canada"    ,"off_language"  : "English"  , "million_sq_km" :  "9.09"},
+    { "country_name" : "Australia" ,"off_language"  : "English"  , "million_sq_km" :  "7.68"},
+    { "country_name" : "China"     ,"off_language"  : "Mandarin" , "million_sq_km" :  "9.33"},
+    { "country_name" : "Kazakhstan","off_language"  : "Kazakh"   , "million_sq_km" :  "2.70"}];
+insert_result = countries.insert_many(countries_data);
+print('multiple insertion: {0}'.format(insert_result.inserted_ids))
 
 
-posts = dtbase.posts;
-post_data = {
-        "author_name"   : "Stepan Serebryakov",
-        "project_title" : "hello, mongoDB",
-    };
-
-result = posts.insert_one(post_data);
-print("one post: {0}".format(result.inserted_id));
 
 
 exit();
